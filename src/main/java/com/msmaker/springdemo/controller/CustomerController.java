1fd7fcc6a4296d46cd82d2fcdb19594391280101
Marco Silva <marcosilva.tec@gamil.com>
2021-06-07 15:01:15 -0300

    Implement Customer Controller & Customer DAO

---
+++ src/main/java/com/msmaker/springdemo/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.msmaker.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping("/list")
+	public String listCustomer(Model topModel) {
+
+		return "list-customers";
+	}
+}
---
+++ src/main/java/com/msmaker/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.msmaker.springdemo.dao;
+
+import java.util.List;
+
+import com.msmaker.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomer();
+}
